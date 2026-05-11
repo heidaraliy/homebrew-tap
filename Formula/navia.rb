68f263573b2db7b17010a3a21849574691299306
Heidar Aliy <99693642+heidaraliy@users.noreply.github.com>
2026-05-11 08:18:53 -0400

    Update navia to v0.1.4

--- Formula/navia.rb
+++ Formula/navia.rb
@@ -1,26 +1,30 @@
+# typed: strict
+# frozen_string_literal: true
+
+# Homebrew formula for Navia.
 class Navia < Formula
   desc "Terminal micro-IDE for project navigation, editing, search, and git review"
   homepage "https://github.com/heidaraliy/navia"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/heidaraliy/navia/releases/download/v0.1.3/navia_0.1.3_darwin_arm64.tar.gz"
-      sha256 "9031a7de80d25fb7ce8dff2778e43d8741386a04916fb05a5afdfad10d42c9b8"
+      url "https://github.com/heidaraliy/navia/releases/download/v0.1.4/navia_0.1.4_darwin_arm64.tar.gz"
+      sha256 "64020d17e163f43555c81dff02af0ad1a77b229834c7f0783d5ace24d8de2d73"
     else
-      url "https://github.com/heidaraliy/navia/releases/download/v0.1.3/navia_0.1.3_darwin_amd64.tar.gz"
-      sha256 "5c5bdf66e8f0324fdd790b067c6188544aa0ec5af0d0376267fa5a31d2be642a"
+      url "https://github.com/heidaraliy/navia/releases/download/v0.1.4/navia_0.1.4_darwin_amd64.tar.gz"
+      sha256 "a3bc1cb89cc0ec939d1c0ed6e42b41c903285778c96710144cfba6e653ac5fd4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/heidaraliy/navia/releases/download/v0.1.3/navia_0.1.3_linux_arm64.tar.gz"
-      sha256 "ca82fbfa531b53ba38cad6e9e639d6d077b78e3a01abc5a0a77b3b171547d5d4"
+      url "https://github.com/heidaraliy/navia/releases/download/v0.1.4/navia_0.1.4_linux_arm64.tar.gz"
+      sha256 "0a1b87c7f5cc3e5f27109b90a9f4371a23b3bde9439bf9905fa34b0c35c1ec39"
     else
-      url "https://github.com/heidaraliy/navia/releases/download/v0.1.3/navia_0.1.3_linux_amd64.tar.gz"
-      sha256 "1306ceb74535451c2b69a230d6e23383599b1be6c7c8d6387b15eb1d15088f1c"
+      url "https://github.com/heidaraliy/navia/releases/download/v0.1.4/navia_0.1.4_linux_amd64.tar.gz"
+      sha256 "e7f0a9cd56c5967123dffa57be46c050efedfca3344491e64965502e537bd3f6"
     end
   end
 
@@ -29,6 +33,6 @@ class Navia < Formula
   end
 
   test do
-    assert_match "navia 0.1.3", shell_output("#{bin}/navia --version")
+    assert_match "navia 0.1.4", shell_output("#{bin}/navia --version")
   end
 end
